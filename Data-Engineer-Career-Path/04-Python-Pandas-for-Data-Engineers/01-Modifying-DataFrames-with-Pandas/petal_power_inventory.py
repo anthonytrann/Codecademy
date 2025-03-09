507380d668566e9209c2666dcdab4bceb9861730
anthonytrann <anthonytran227@gmail.com>
2025-03-09 21:00:22 +0000

    small changes

--- Data-Engineer-Career-Path/04-Python-Pandas-for-Data-Engineers/01-Modifying-DataFrames-with-Pandas/petal_power_inventory.py
+++ Data-Engineer-Career-Path/04-Python-Pandas-for-Data-Engineers/01-Modifying-DataFrames-with-Pandas/petal_power_inventory.py
@@ -1,24 +1,22 @@
 import codecademylib3
 import pandas as pd
 
-inventory = pd.read_csv("inventory.csv")
+inventory = pd.read_csv('inventory.csv')
 
 staten_island = inventory.head(10)
 
-product_request = staten_island["product_description"]
+product_request = staten_island['product_description']
 
-seed_request = inventory[
-    (inventory["location"] == "Brooklyn") & (inventory["product_type"] == "seeds")
-]
+seed_request = inventory[(inventory['location'] == 'Brooklyn') & (inventory['product_type'] == 'seeds')]
 
-inventory["in_stock"] = inventory["quantity"].apply(
-    lambda value: True if value > 0 else False
-)
+inventory['in_stock'] = inventory['quantity'].apply(lambda value: True if value > 0 else False)
 
-inventory["total_value"] = inventory["price"] * inventory["quantity"]
+inventory['total_value'] = inventory['price'] * inventory['quantity']
 
 
-combine_lambda = lambda row: "{} - {}".format(row.product_type, row.product_description)
-inventory["full_description"] = inventory.apply(combine_lambda, axis=1)
+combine_lambda = lambda row: \
+    '{} - {}'.format(row.product_type,
+                     row.product_description)
+inventory['full_description'] = inventory.apply(combine_lambda, axis=1)
 
 print(inventory)
